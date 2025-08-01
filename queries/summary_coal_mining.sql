49af87322049a351eaf5efaee0936ee57c2f6d84
Widi Afandi <widi.afandi@batumbu.id>
2025-08-01 16:08:28 +0700

    feat: add generate summary_coal_mining and the validation queries

--- queries/summary_coal_mining.sql
+++ queries/summary_coal_mining.sql
@@ -1,3 +1,10 @@
+/*
+
+Generate summary data of coal mining
+
+*/
+drop table if exists layer_gold.summary_coal_mining;
+create table layer_gold.summary_coal_mining engine = MergeTree() order by _id as
 with src_summary_production_logs as (
 	select
 		date,
@@ -16,9 +23,14 @@ src_summary_equipment_sensor as (
 ),
 src_daily_weather as (
 	select
-		arrayJoin(JSONExtract(adw.daily, 'time', 'Array(Date)')) as date,
-		arrayJoin(JSONExtract(adw.daily, 'temperature_2m_mean', 'Array(Float64)')) as temperature_2m_mean,
-		arrayJoin(JSONExtract(adw.daily, 'precipitation_sum', 'Array(Float64)')) as precipitation_sum
+		arrayJoin(arrayZip(
+	        JSONExtract(adw.daily, 'time', 'Array(Nullable(Date))'),
+	        JSONExtract(adw.daily, 'temperature_2m_mean', 'Array(Nullable(Float64))'),
+	        JSONExtract(adw.daily, 'precipitation_sum', 'Array(Nullable(Float64))')
+	    )) as zip,
+	    zip.1 as date,
+	    zip.2 as temperature_2m_mean,
+	    zip.3 as precipitation_sum
 	from layer_bronze.api_daily_weather adw
 ),
 transformation as (
@@ -28,7 +40,11 @@ transformation as (
 		sspl.average_quality_grade,
 		sses.equipment_utilization,
 		floor((sses.total_fuel_consumption / sspl.total_production_daily), 3) as fuel_efficiency,
-		sdw.temperature_2m_mean as weather_impact
+		case 
+            when sdw.precipitation_sum = 0 then 'non-rainy'
+            when  sdw.precipitation_sum > 0 then 'rainy'
+            else null 
+        end as weather_impact
 	from src_summary_production_logs sspl
 	left join src_summary_equipment_sensor sses
 	using(date)
@@ -37,14 +53,16 @@ transformation as (
 ),
 main as (
 	select 
+		generateUUIDv4() as _id,
 		date,
 		total_production_daily,
 		average_quality_grade,
 		equipment_utilization,
 		fuel_efficiency,
-		weather_impact
+		weather_impact,
+		now() as _created_at
 	from transformation t
 	where 1=1
 	order by date
 )
-select * from main 
+select * from main;
